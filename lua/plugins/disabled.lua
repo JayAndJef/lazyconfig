d34c0283e6f82e4794b1911f3fdffe41135f5c9e
JayAndJef <109392310+JayAndJef@users.noreply.github.com>
2024-04-13 11:36:10 -0700

    initial commit

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/noice.nvim",
+    enabled = false,
+    config = {
+      cmdline = {
+        enabled = false,
+      },
+    },
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "ggandor/flit.nvim",
+    enabled = false,
+  },
+}
---
+++ lua/plugins/qol.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "pocco81/auto-save.nvim",
+    lazy = false,
+    trigger_events = { "InsertLeave", "TextChanged" },
+  },
+}
